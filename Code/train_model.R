b00e5c0982387fa192a5452f89cd2caebfc3f2ae
Ting <tinghf@gmail.com>
2015-08-22 21:48:47 -0700

    update script to work with quadgram and train model for it

--- Code/train_model.R
+++ Code/train_model.R
@@ -13,3 +13,15 @@ tri_naiveBayes <-
 # save the model
 save(tri_naiveBayes, unigram_levels, file = "tri_naiveBayes.RData")
 
+
+# load the trigram data
+load("df_quadgram.RData")
+
+# e1071 package
+quad_naiveBayes <- 
+  naiveBayes( Y ~ X1 + X2 + X3 ,
+              df_quadgram )
+
+# save the model
+save(quad_naiveBayes, unigram_levels, file = "quad_naiveBayes.RData")
+
